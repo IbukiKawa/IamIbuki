e25e45b85cd23b869a962d2d1dd0d4206ae0fe5b
IbukiKawa <jadsajd1234@gmail.com>
2025-11-09 16:55:45 +0900

    darkmode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import awsAmplify from "astro-aws-amplify";
 
-// https://astro.build/config
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-    site: "https://main.d1d7i8gxj6sut0.amplifyapp.com/",
-    adapter: awsAmplify(),
+  site: "https://main.d1d7i8gxj6sut0.amplifyapp.com/",
+  integrations: [preact()],
 });
-    
