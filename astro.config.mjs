b86ee574d5f54e7cc4167c30c6ab99dce89e6a6e
IbukiKawa <jadsajd1234@gmail.com>
2025-11-09 16:13:44 +0900

    URLの変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+    site: "hhttps://main.d1d7i8gxj6sut0.amplifyapp.com/",
     adapter: awsAmplify(),
 });
     
