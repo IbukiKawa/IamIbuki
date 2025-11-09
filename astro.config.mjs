1a15e8c33937bbdedf3e828162da1e60b8039e8e
IbukiKawa <jadsajd1234@gmail.com>
2025-11-09 15:53:40 +0900

    rssの準備とAMplifyの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import awsAmplify from "astro-aws-amplify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://example.com",
+    adapter: awsAmplify(),
+});
+    
